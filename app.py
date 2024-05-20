a95595bc1ad0fc2d30be8eb401130c3822314879
cactus211107 <jackrushmore5@gmail.com>
2024-05-20 22:30:28 +0200

    added about page, no content

--- app.py
+++ app.py
@@ -105,6 +105,9 @@ def slide_download_progress_api(id):
     if os.path.exists(slides_folder):return {"status":"downloading","slides_downloaded":len(os.listdir(slides_folder))}
     else:return {"status":"non-existant folder"}
 
+@app.root('/about')
+def about_page():
+    return render_template('about.html')
 
 @app.route('/robots.txt')
 def robots_txt():return send_file('robots.txt')
